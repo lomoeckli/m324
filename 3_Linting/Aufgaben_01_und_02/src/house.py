71856977199bfa6c0f1ff33c8933396e46d1c8f9
lomoeckli <louis_moeckli@sluz.ch>
2025-11-23 18:54:48 +0100

    Add Aufgabe_02 linting

--- 3_Linting/Aufgaben_01_und_02/src/house.py
+++ 3_Linting/Aufgaben_01_und_02/src/house.py
@@ -2,6 +2,20 @@ class House:
     def __init__(self):
         print("New house was builded")
     
+    def login(self):
+        username = "admin"
+        password = "admin"
+
+        input_username = input("Enter username: ")
+        input_password = input("Enter password: ")
+
+        if input_username == username and input_password == password:
+            print("Login successful")
+            return True
+        else:
+            print("Login failed")
+            return False
+
     def get_name(self):
         print(self.name)
         return self.name
--- 3_Linting/Aufgaben_01_und_02/src/main.py
+++ 3_Linting/Aufgaben_01_und_02/src/main.py
@@ -2,5 +2,6 @@ from house import House
 
 villa = House()
 
+villa.login()
 villa.set_name("Neverland")
 villa.get_name()
