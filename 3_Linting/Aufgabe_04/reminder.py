91f200af7184a4e86cbf6cbaf0b34fe5ce1a2f44
lomoeckli <louis_moeckli@sluz.ch>
2025-11-23 19:57:28 +0100

    Add Aufgabe_04 linting

---
+++ 3_Linting/Aufgabe_04/reminder.py
@@ -0,0 +1,7 @@
+"""This module contains a function that returns an important reminder."""
+
+def get_reminder():
+    """Returns an important reminder."""
+    return "Important reminder"
+
+print(get_reminder())
